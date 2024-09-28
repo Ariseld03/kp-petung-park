ec70229c3e21851724b705f2ec64cf5e60c5ff10
MFeliciaH <s160821025@student.ubaya.ac.id>
2024-09-28 14:36:51 +0700

    hidangan

--- resources/views/hidangan.blade.php
+++ resources/views/hidangan.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('page-css')
-    <link rel="stylesheet" href="{{ asset('/css/kategori.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/hidangan.css') }}">
 @endsection
 
 @section('container-main')
@@ -11,6 +11,17 @@
             <a href="{{ url('kategori') }}" class="btn-kembali">Kembali</a>
             <h2>Kategori Kuliner</h2>
         </div>
-        <img src="https://via.placeholder.com/300x300" alt="hidangan" class="hidangan-gambar">
+        <div class="hidangan-container">
+            <img src="https://via.placeholder.com/300x300" alt="hidangan" class="hidangan-gambar">
+            <div class="hidangan-detail">
+                <h3>Judul Hidangan</h3>
+                <p>Deskripsi hidangan.</p>
+                <p class="harga">Harga: Rp. 50.000</p>
+                <p class="rekomendasi">Rekomendasi</p>
+                <button class="like-button">
+                    ❤️
+                </button>
+            </div>
+        </div>
     </div>
 @endsection
