b16d5579f4468dfeff128d42df44de5404a550b8
MFeliciaH <s160821025@student.ubaya.ac.id>
2024-10-02 19:55:38 +0700

    agenda + artikel (UI)

--- resources/views/kegiatanLalu.blade.php
+++ resources/views/kegiatanLalu.blade.php
@@ -6,42 +6,37 @@
 
 @section('container-main')
     <div class="kegiatan-detail">
-        <h2>Detail Kegiatan Lalu</h2>
+        <a href="{{ url()->previous() }}" class="btn-kembali">Kembali</a>
+        <h2>{{ $agenda->event_name }}</h2>
+
+        <div class="gallery">
+            @foreach($galleries as $gallery)
+                <div class="gallery-item">
+                    <img src="{{ $gallery->photo_link }}" alt="{{ $gallery->name }}" class="gallery-img">
+                    <p>{{ $gallery->description }}</p>
+                    <span class="like-icon">❤️ {{ $gallery->number_love }}</span>
+                </div>
+            @endforeach
+        </div>
 
         <div class="kegiatan-info">
-            <p><strong>Nama Kegiatan:</strong> {{ $agenda->event_name }}</p>
             <p><strong>Tanggal:</strong> {{ \Carbon\Carbon::parse($agenda->event_start_date)->format('d/m/Y') }} - {{ \Carbon\Carbon::parse($agenda->event_end_date)->format('d/m/Y') }}</p>
             <p><strong>Lokasi:</strong> {{ $agenda->event_location }}</p>
             <p><strong>Deskripsi:</strong> {{ $agenda->description }}</p> <!-- Ganti event_description dengan description -->
         </div>
 
-        <h3>Artikel Terkait</h3>
-        @if($articles->isEmpty())
-            <p>Tidak ada artikel terkait.</p>
-        @else
-            @foreach($articles as $article)
-                <div class="article">
-                    <h4>{{ $article->title }}</h4>
-                    <p>{{ $article->main_content }}</p>
-
-                    <h5>Galeri:</h5>
-                    @if($galleries->isEmpty())
-    <p>Tidak ada galeri yang tersedia.</p>
-@else
-                        <div class="gallery">
-                            @foreach($galleries as $gallery)
-                                <div class="gallery-item">
-                                    <img src="{{ $gallery->photo_link }}" alt="{{ $gallery->name }}">
-                                    <p>{{ $gallery->description }}</p>
-                                    <p>Likes: {{ $gallery->number_love }}</p>
-                                </div>
-                            @endforeach
-                        </div>
-                    @endif
-                </div>
-            @endforeach
-        @endif
-
-        <button onclick="window.history.back()">Kembali</button>
+        <div class="artikel">
+            <h3>Artikel Terkait</h3>
+            @if($articles->isEmpty())
+                <p>Tidak ada artikel terkait.</p>
+            @else
+                @foreach($articles as $article)
+                    <div class="article">
+                        <h4>{{ $article->title }}</h4>
+                        <p>{{ $article->main_content }}</p>
+                    </div>
+                @endforeach
+            @endif
+        </div>
     </div>
 @endsection
