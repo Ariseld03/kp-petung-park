e52e825c1f70f775dda1648ad1a7a869c134439a
MFeliciaH <s160821025@student.ubaya.ac.id>
2024-11-03 15:01:22 +0700

    typo button

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -20,7 +20,7 @@
                     <li class="nav-item"><a class="nav-link" href="{{ url('/staffShow') }}">Staff</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ url('/galeri') }}">Galeri</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ url('/menu') }}">Menu</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/wisata') }}">Wisata</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ url('/wisataStaff') }}">Wisata</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ url('/kegiatan') }}">Kegiatan</a></li>
                 </ul>
             </div>
