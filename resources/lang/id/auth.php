8be6442966e0143219891dd7a5476363a52af79e
Amelia Griselda <griseldaamelia681@gmail.com>
2025-02-11 23:32:49 +0700

    New Message Custom Indo

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'failed' => 'Kredensial ini tidak cocok dengan data kami.',
+    'password' => 'Kata sandi yang dimasukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+    'verify' => 'Silakan verifikasi email Anda sebelum melanjutkan.',
+    'verification_sent' => 'Kami telah mengirimkan tautan verifikasi ke email Anda.',
+];
+
+?>
