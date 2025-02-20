b66c1313c22d8815abc5eab9cd3418f0fc8f0e56
Amelia Griselda <griseldaamelia681@gmail.com>
2025-02-20 11:39:06 +0700

    Revisi Layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,9 +8,7 @@
     <title>@yield('title', 'Petung Park')</title>
     <link rel="icon" href="/images/footer/logoPetungPark.png" type="image/x-icon">
     <!-- Link Bootstrap CSS -->
-    {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     @yield('page-css')
     <style>
         /* Custom CSS untuk styling yang tidak ada di Bootstrap */
@@ -25,9 +23,8 @@
     @include('layouts.footer')
     <!-- Link Bootstrap JS dan jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     @yield('page-js')
 
 </body>
